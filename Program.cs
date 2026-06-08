de6c96384a919a08630ab5ffe447eee5e05ec1a6
Hidaya21 <alabrihidaya@gmail.com>
2026-06-08 10:23:31 +0400

    Update Booking

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ namespace TSDSCAP01
         static List<string> ticketNumbers = new List<string> { "TKT-1", "TKT-2", "TKT-3" };
         static List<string> cancelledTickets = new List<string>();
         static string[] flightNumbers = { "FL-001", "FL-002", "FL-003" };
-        static List<string> availableDates = new List<string> {"2026-06-10","2026-06-11","2026-06-12" };
+        static List<string> availableDates = new List<string> { "2026-06-10", "2026-06-11", "2026-06-12" };
         static Dictionary<string, string> bookingRecord = new Dictionary<string, string>();
 
-        
+
 
         public static void RegisterNewPassenger()
         {
@@ -99,7 +99,7 @@ namespace TSDSCAP01
                 return;
             }
             //Check booking record in dictionary
-            if (!bookingRecord.ContainsKey(ticket))
+            if (bookingRecord.ContainsKey(ticket))
             {
                 Console.WriteLine("No booking found for this ticket.");
                 return;
@@ -107,7 +107,7 @@ namespace TSDSCAP01
             Console.WriteLine("Flights: ");
             for (int i = 0; i < flightNumbers.Length; i++)
             {
-                Console.WriteLine(i + ": " + flightNumbers[i]);
+                Console.WriteLine((i + 1) + ": " + flightNumbers[i]);
             }
             // Ask user to select a flight
             Console.Write("Select Flight: ");
@@ -149,7 +149,6 @@ namespace TSDSCAP01
             Console.WriteLine("Date: " + selectedDate);
 
         }
-
         public static void ViewBookingDetails()
         {
             Console.Write("Enter your ticket: ");
@@ -180,7 +179,6 @@ namespace TSDSCAP01
             // Retrieve and split booking details
             string bookingInfo = bookingRecord[ticketID];
             string[] parts = bookingInfo.Split('|');
-
             string flightNumber = parts[0];
             string date = parts[1];
 
@@ -191,57 +189,212 @@ namespace TSDSCAP01
             Console.WriteLine("Flight Number  : " + flightNumber);
             Console.WriteLine("Date           : " + date);
         }
-        static void Main(string[] args)
+        public static void UpdateBooking()
         {
-            Console.WriteLine("================================= ");
-            Console.WriteLine("SKY WINGS FLIGHT MANAGEMENT SYSTEM");
-            Console.WriteLine("================================= ");
-            Console.WriteLine("1. Register New Passenger ");
-            Console.WriteLine("2. View All Passengers ");
-            Console.WriteLine("3. Book a Flight Ticke   ");
-            Console.WriteLine("4. View Booking Details ");
-            Console.WriteLine("5. Update a Booking   ");
-            Console.WriteLine("6. Cancel a Ticket ");
-            Console.WriteLine("7. Passenger Check-In");
-            Console.WriteLine("8. Board Passengers (Boarding Stack) ");
-            Console.WriteLine("9. Generate Flight Manifest ");
-            Console.WriteLine("10.Manage Waitlist & Seat Assignment");
-            Console.WriteLine("0. Exit");
-            Console.WriteLine("================================= ");
+            Console.Write("Enter Ticket ID: ");
+            string ticket = Console.ReadLine();
+            // Check ticket exists
+            if (!ticketNumbers.Contains(ticket))
+            {
+                Console.WriteLine("Ticket ID does not exist");
+                return;
+            }
+            // Check ticket is not cancelled
+            if (cancelledTickets.Contains(ticket))
+            {
+                Console.WriteLine("This ticket has been cancelled");
+                return;
+            }
+            //Check booking exists
+            if (!bookingRecord.ContainsKey(ticket))
+            {
+                Console.WriteLine("No booking found for this ticket");
+                return;
+            }
+            //Extract current booking
+            string bookingInfo = bookingRecord[ticket];
+            string[] parts = bookingInfo.Split('|');
+
+            string oldFlight = parts[0];
+            string oldDate = parts[1];
+
+            //Display current booking
+            Console.WriteLine("Current Booking:");
+            Console.WriteLine("Flight: " + oldFlight);
+            Console.WriteLine("Date: " + oldDate);
+
+            // Menu
+            Console.WriteLine("Choose update option:");
+            Console.WriteLine("1. Change Flight Only");
+            Console.WriteLine("2. Change Date Only");
+            Console.WriteLine("3. Change Both");
+            Console.WriteLine("0. Cancel Update");
+
             Console.Write("Enter your choice: ");
             int choice = int.Parse(Console.ReadLine());
-            switch (choice)
+
+            if (choice == 0)
             {
-                case 1:
-                    RegisterNewPassenger();
-                    break;
-                case 2:
-                    ViewAllPassengers();
-                    break;
-                case 3:
-                    BookFlightTicke();
-                    break;
-                case 4:
-                    ViewBookingDetails();
-
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                case 0:
-                    break;
-
-
+                Console.WriteLine("Update cancelled.");
+                return;
             }
-         }
+            string newFlight = oldFlight;
+            string newDate = oldDate;
+            // Change flight only
+            if (choice == 1)
+            {
+                Console.WriteLine("Available Flights:");
+                for (int i = 0; i < flightNumbers.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + ": " + flightNumbers[i]);
+                }
+
+                Console.Write("Select flight: ");
+                int flightChoice = int.Parse(Console.ReadLine());
+
+                if (flightChoice < 1 || flightChoice > flightNumbers.Length)
+                {
+                    Console.WriteLine("Invalid flight choice");
+                    return;
+                }
+
+                newFlight = flightNumbers[flightChoice - 1];
+            }
+            // Change date only
+            else if (choice == 2)
+            {
+                Console.WriteLine("Available Dates:");
+                for (int i = 0; i < availableDates.Count; i++)
+                {
+                    Console.WriteLine((i + 1) + ": " + availableDates[i]);
+                }
+                Console.Write("Select date: ");
+                int dateChoice = int.Parse(Console.ReadLine());
+
+                if (dateChoice < 1 || dateChoice > availableDates.Count)
+                {
+                    Console.WriteLine("Invalid date choice.");
+                    return;
+                }
+
+                newDate = availableDates[dateChoice - 1];
+            }
+            // Change both
+            else if (choice == 3)
+            {
+                Console.WriteLine("Available Flights:");
+                for (int i = 0; i < flightNumbers.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + ": " + flightNumbers[i]);
+                }
+
+                Console.Write("Select flight: ");
+                int flightChoice = int.Parse(Console.ReadLine());
+
+                if (flightChoice < 1 || flightChoice > flightNumbers.Length)
+                {
+                    Console.WriteLine("Invalid flight choice.");
+                    return;
+                }
+
+                newFlight = flightNumbers[flightChoice - 1];
+
+                Console.WriteLine("Available Dates:");
+                for (int i = 0; i < availableDates.Count; i++)
+                {
+                    Console.WriteLine((i + 1) + ": " + availableDates[i]);
+                }
+                Console.Write("Select date: ");
+                int dateChoice = int.Parse(Console.ReadLine());
+
+                if (dateChoice < 1 || dateChoice > availableDates.Count)
+                {
+                    Console.WriteLine("Invalid date choice");
+                    return;
+                }
+                newDate = availableDates[dateChoice - 1];
+            }
+            else
+            {
+                Console.WriteLine("Invalid menu option");
+                return;
+            }
+
+            //Update dictionary 
+            bookingRecord[ticket] = newFlight + "|" + newDate;
+
+            //Confirmation
+            Console.WriteLine("Booking Updated Successfully!");
+
+            Console.WriteLine("OLD BOOKING:");
+            Console.WriteLine("Flight: " + oldFlight);
+            Console.WriteLine("Date: " + oldDate);
+            Console.WriteLine("NEW BOOKING:");
+            Console.WriteLine("Flight: " + newFlight);
+            Console.WriteLine("Date: " + newDate);
+
+
+        }
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (exit == false)
+            {
+                Console.WriteLine("================================= ");
+                Console.WriteLine("SKY WINGS FLIGHT MANAGEMENT SYSTEM");
+                Console.WriteLine("================================= ");
+                Console.WriteLine("1. Register New Passenger ");
+                Console.WriteLine("2. View All Passengers ");
+                Console.WriteLine("3. Book a Flight Ticke   ");
+                Console.WriteLine("4. View Booking Details ");
+                Console.WriteLine("5. Update a Booking   ");
+                Console.WriteLine("6. Cancel a Ticket ");
+                Console.WriteLine("7. Passenger Check-In");
+                Console.WriteLine("8. Board Passengers (Boarding Stack) ");
+                Console.WriteLine("9. Generate Flight Manifest ");
+                Console.WriteLine("10.Manage Waitlist & Seat Assignment");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("================================= ");
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        RegisterNewPassenger();
+                        break;
+                    case 2:
+                        ViewAllPassengers();
+                        break;
+                    case 3:
+                        BookFlightTicke();
+                        break;
+                    case 4:
+                        ViewBookingDetails();
+                        break;
+                    case 5:
+                        UpdateBooking();
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit");
+                        exit = true;
+                        break;
+
+
+                }
+                Console.Write(" press any key to countinue...  ");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
     }
 }
